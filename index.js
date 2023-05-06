71d78231b652ed8db2317f7f045d22d2826282cd
JohanFernandoLema <johan.13fernando@gmail.com>
2023-05-06 12:00:21 -0400

    object added to javaScript, afterwards item will be added dynamically to html

---
+++ index.js
@@ -0,0 +1,9 @@
+const meals = [
+  {
+    img: './Assets/images/meal1.png',
+    name: 'the big chill cakery',
+    desc: 'shop 1, plot d, samruddhi complex, chincholi',
+    cusine: 'bakery',
+    cost: 700,
+  },
+]
