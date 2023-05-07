65d827acf0b408dffa13c8fa16772a4d0de4b8c4
JohanFernandoLema <johan.13fernando@gmail.com>
2023-05-07 16:11:47 -0400

    adding products to showcase in our html file

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const meals = [
   {
-    id:1,
+    id: 1,
     img: './Assets/images/meal1.png',
     name: 'the big chill cakery',
     desc: 'shop 1, plot d, samruddhi complex, chincholi',
     cusine: 'bakery',
     cost: 700,
   },
+  {
+    id: 2,
+    img: './Assets/images/banana.jpg',
+    name: 'banana split',
+    desc: 'banana with two scoopes of ice-cream and cherries on top with icing and sprincles',
+    cusine: 'bakery',
+    cost: 1200,
+  },
 ]
-const itemsInformation = document.querySelector('.item-info');
+const itemsInformation = document.querySelector('.item-info')
 
-window.addEventListener('DOMContentLoaded', ()=>{
-    let displayMenu = meals.map((items)=>{
-        return `<div class="picture">
+window.addEventListener('DOMContentLoaded', () => {
+  let displayMenu = meals.map((items) => {
+    return `<div class="picture">
         <img src=${items.img}>
         </div>
         <div class="item-desc">
@@ -23,19 +31,18 @@ window.addEventListener('DOMContentLoaded', ()=>{
         <h3>CUISINE:</h3>
         <span>${items.cusine}</span>
         <h3>COST FOR TWO:</h3>
-        <span>₹${items.cost}</span>`;
-    });
-    displayMenu = displayMenu.join('');
-    itemsInformation.innerHTML = displayMenu;
-    console.log(displayMenu);
-});
-
+        <span>₹${items.cost}</span>`
+  })
+  displayMenu = displayMenu.join('')
+  itemsInformation.innerHTML = displayMenu
+  console.log(displayMenu)
+})
 
 // Collapsible button
-const navToggle =  document.querySelector('.filterSlideIn');
-const asideSlide = document.querySelector('aside');
+const navToggle = document.querySelector('.filterSlideIn')
+const asideSlide = document.querySelector('aside')
 
-navToggle.addEventListener("click", function(){
+navToggle.addEventListener('click', function () {
   // if(asideSlide.classList.contains("active")){
   //   asideSlide.classList.remove("active");
   // }
@@ -43,5 +50,5 @@ navToggle.addEventListener("click", function(){
   //   asideSlide.classList.add("active");
   // }
   //Shorhand for code above
-  asideSlide.classList.toggle('active');
+  asideSlide.classList.toggle('active')
 })
