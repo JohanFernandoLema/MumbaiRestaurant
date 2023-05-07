96a61cc2e9784ce4a03bc8a8c87f045da81166f7
JohanFernandoLema <johan.13fernando@gmail.com>
2023-05-07 09:44:48 -0400

    items added dynamically through js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const meals = [
   {
+    id:1,
     img: './Assets/images/meal1.png',
     name: 'the big chill cakery',
     desc: 'shop 1, plot d, samruddhi complex, chincholi',
@@ -7,3 +8,24 @@ const meals = [
     cost: 700,
   },
 ]
+const itemsInformation = document.querySelector('.item-info');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    let displayMenu = meals.map((items)=>{
+        return `<div class="picture">
+        <img src=${items.img}>
+        </div>
+        <div class="item-desc">
+        <h2>${items.name}</h2>
+        <h4>FORT</h4>
+        <p>${items.desc}</p>
+        </div>  
+        <h3>CUISINE:</h3>
+        <span>${items.cusine}</span>
+        <h3>COST FOR TWO:</h3>
+        <span>₹${items.cost}</span>`;
+    });
+    displayMenu = displayMenu.join('');
+    itemsInformation.innerHTML = displayMenu;
+    console.log(displayMenu);
+});
